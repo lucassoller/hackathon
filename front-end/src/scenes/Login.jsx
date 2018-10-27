51a12e71e61333cd6a9e4ea7f608fc9f64a8b100
Guilherme Francisco <guisanfrancisco@gmail.com>
2018-10-27 02:54:49 -0200

    userservice centralizado

--- front-end/src/scenes/Login.jsx
+++ front-end/src/scenes/Login.jsx
@@ -2,6 +2,43 @@ import React, { Component, Fragment } from 'react'
 import { Input, Form } from '../components/index'
 
 export default class Login extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      senha: '',
+      logged: false
+    }
+  }
+
+  componentDidMount() {
+    this.userService = new UserService()
+    // if (this.userService.getToken()) {
+    //   this.setState({ logged: true })
+    // }
+  }
+
+  handleChange = (event) => {
+    const target = event.target
+    const name = target.name
+    this.setState({
+      [name]: target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+
+    this.userService.login(this.state.email, this.state.senha).then((response) => {
+      this.userService.salvarToken(response.data.token)
+      this.setState({
+        logged: true
+      })
+    })
+  }
+
+
   render() {
     return (
       <Fragment>
--- front-end/src/services/user.service.js
+++ front-end/src/services/user.service.js
@@ -6,5 +6,42 @@ export default class UserService extends BaseService {
     constructor(){
         super();
         this.baseUserUrl = `${this.baseUrl}usuario/`
+        this.TOKEN_KEY = 'TOKEN'
     }
+
+    login(email, senha){
+        const usuarioDto = {
+            email: email,
+            senha: senha
+        }
+        return axios.post(`${this.baseUrl}public/login`, usuarioDto);
+    }
+
+    registrar(nomeCompleto, email, senha, imagem, telefone){
+        const usuarioDto = {
+            nomeCompleto: nomeCompleto,
+            email: email,
+            senha: senha,
+            imagem: imagem,
+            telefone: telefone
+        }
+        return axios.post(`${this.baseUrl}public/registro`, usuarioDto);
+    }
+
+    salvarToken(token) {
+        localStorage.setItem(this.TOKEN_KEY, token)
+    }
+
+    getToken() {
+        return localStorage.getItem(this.TOKEN_KEY)
+    }
+
+    /*getUser() {
+        return axios.get(`${this.baseUrl}public/login`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${this.getToken()}`
+                }
+            })
+    }*/
 }
