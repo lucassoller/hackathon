70eeda3bedda09b302da73c495b09a99974de55a
Yasmin Moraes <yasminmoraes@MacBook-de-Yasmin.local>
2018-10-27 06:58:08 -0200

    Header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
 import {Cadastro, Login, Home} from './scenes/index'
+import Header from './components/header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Header/>
         <Route component={Home} path="/" exact/>
         <Route component={Login} path="/login" />
         <Route component={Cadastro} path="/cadastro" />
