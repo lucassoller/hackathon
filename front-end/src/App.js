4b8de7e7b871e38c89dbe05a30f7a81b7b86d0e8
Guilherme Francisco <guisanfrancisco@gmail.com>
2018-10-27 05:02:02 -0200

    Rotas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom'
 import {Cadastro, Login} from './scenes/index'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Route component={Home} path="/" exact/>
+        <Route component={Login} path="/login" />
+        <Route component={Cadastro} path="/cadastro" />
           <Login />
       </div>
     );
---
+++ front-end/src/components/mapa/Map.jsx
@@ -0,0 +1,66 @@
+import React, { Component, Fragment } from 'react'
+import './Map.css'
+import GoogleMapReact from 'google-map-react';
+
+const MapPointer = ({ text }) => (
+    <div style={{
+        color: 'white', 
+        fontWeight: 'bold',
+        background: 'rgba(255, 0, 0,0.5)',
+        display: 'inline-flex',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '100%',
+        transform: 'translate(-50%, -50%)',
+        width: '100px',
+        height: '100px'
+
+      }}>
+        {text}
+      </div>
+  );
+
+export default class Map extends Component {
+
+    componentDidMount() {
+        this.actualPosition()
+    }
+
+
+    constructor(props) {
+        super()
+        this.state = {
+            lat: 59.95,
+            lng: 30.33
+        }
+    }
+
+    actualPosition = () => {
+        navigator.geolocation.getCurrentPosition(position => {
+            this.setState({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="map-container">
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyCymW1VwK90lGRjoMD4xqNN8KgIE_RPOzU' }}
+                    defaultCenter={{lat: this.state.lat, lng: this.state.lng}}
+                    defaultZoom={14}
+                >
+                    <MapPointer
+                        lat={this.state.lat}
+                        lng={this.state.lng}
+                        text={'AJUDE-ME!'}
+                    />
+                </GoogleMapReact>
+            </div>
+        )
+    }
+}
+
