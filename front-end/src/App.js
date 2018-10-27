984629819e01f7b9f3a7185c0f1ab79d2c14fef8
Eduardo Cristiano Thums <eduardo.thums@cwi.com.br>
2018-10-27 00:23:25 -0200

    Adicionando tela de cadastro

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Input, Form} from './components/index'
+import { Input, Form } from './components/index'
 import Sessao from './components/sessao/Sessao'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-      {/* <Form>
-        <Input descricao = "Aqui vai um input" />
-      </Form> */}
-      <Sessao titulo='Perfil' nomeBotao='Editar'/>
+        <Form titulo = 'Cadastro'>
+          <Input descricao="Nome" />
+          <Input descricao="Senha" />
+          <Input descricao="Confirmar senha   " />
+          <Input descricao="Email" />
+          <Input descricao="Telefone" />
+        </Form>
+        {/* <Sessao titulo='Perfil' nomeBotao='Editar'/> */}
       </div>
     );
   }
